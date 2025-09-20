c28ffcb49635e7075803637a8d15308aae865ae9
Luz De Leon <aracely.des@uanl.edu.mx>
2025-09-20 11:02:02 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//====Variables=====//
+var arreglo = [1, 2, 3, 4, 5];
+var entero = 1;
+var cadena_texto = "Hola Mundo";
+var booleano = true;
+
+//===Constantes====//
+const PI = 3.1416;
+
+
+//====Funciones====//
+function suma(x, y){
+    var resultado = x + y;
+    console.log("El resultado de la suma es: " + resultado);
+    return resultado;
+}
+
+function imprimirMensaje(mensaje){
+    console.log("Este es mi mensaje " + mensaje);
+}
+
+imprimirMensaje("Hola");
+imprimirMensaje("Vacaciones 1 semana :D");
+imprimirMensaje(true);
+
+suma(1, 2);
+suma(10, 20);
+suma(100, 200);
+
+
+//===Elementos de la pagina como variables===///
+var botonBuscar = document.getElementById("buscar");
+
+console.log(botonBuscar);
+
+//==== Eventos===== //
+botonBuscar.addEventListener("click", function(){
+    //alert("Hiciste clic al boton");
+    botonBuscar.classList.add("rojo");
+});
+
